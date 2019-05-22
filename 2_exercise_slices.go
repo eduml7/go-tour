825dc384a69e9c41519b04eab245f11055eb85a1
Edu <egonzalezs@gmail.com>
2019-05-22 12:41:06 +0200

    Create 2_exercise_slices.go

---
+++ 2_exercise_slices.go
@@ -0,0 +1,33 @@
+/*
+Implement Pic. It should return a slice of length dy, each element of which is a slice of dx 8-bit unsigned integers. When you run the program, it will display your picture, interpreting the integers as grayscale (well, bluescale) values.
+The choice of image is up to you. Interesting functions include (x+y)/2, x*y, and x^y.
+(You need to use a loop to allocate each []uint8 inside the [][]uint8.)
+(Use uint8(intValue) to convert between types.) 
+*/
+
+package main
+
+import "golang.org/x/tour/pic"
+
+
+func Pic(dx, dy int) [][]uint8 {
+	lol := make([][]uint8, dy)
+
+	for y := range lol {
+
+		lol[y] = make([]uint8, dx)
+
+		for x := range lol[y] {
+			lol[y][x] = uint8(x^y)
+			//lol[y][x] = uint8((x+y)/2)
+			//lol[y][x] = uint8(x*y)
+		}
+	}
+
+	return lol
+}
+
+func main() {
+	pic.Show(Pic)
+}
+
